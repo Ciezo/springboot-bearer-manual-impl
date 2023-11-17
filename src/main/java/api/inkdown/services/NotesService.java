93623a75b106155dc673ff385faf9b42c85ea353
Cloyd Van <abcd123.letters@gmail.com>
2023-11-17 17:51:27 +0800

    Patch update operation
    Implemented a better way of setting up an existing note
     - 1. Check if the record exists, if not create one based on PK and FK of the resource
     - 2. Creates an instance of the Note model and sets all its attributes

--- src/main/java/api/inkdown/services/NotesService.java
+++ src/main/java/api/inkdown/services/NotesService.java
@@ -7,6 +7,7 @@ import api.inkdown.repository.NotesRepository;
 import api.inkdown.interfaces.INotesService;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -27,22 +28,45 @@ public class NotesService implements INotesService {
 
     @Override
     public Notes updateNote(Notes note, long note_id, long user_id) {
+        /**
+         * @note Must always provide note_id and user_id on the client side
+         * @param note, the Notes entity model
+         * @param note_id, the existing id to identify note resource
+         * @param user_id, another existing id to identify the user of the note (foreign key)
+         */
 
         // Find the note to update
         Notes noteToUpdate = repository.findById(note_id).get();
-        long tempNoteID = noteToUpdate.getNote_id();
 
-        // Identify the user logged-in for their note to update
-        long userInSessionID = repository.findById(user_id).get().getUser_id();
+        // Ensure the note object returned is not null
+        if (Objects.nonNull(noteToUpdate)) {
+            System.out.println("Record found!");
 
-        if(user_id == userInSessionID) {
-            System.out.println("Found user in session!");
-            if(note_id == tempNoteID) {
-                System.out.println("Identified note resource...updating");
-                return repository.save(note);
-            }
+            System.out.println("Setting attributes");
+            noteToUpdate.setNote_id(note.getNote_id());
+            noteToUpdate.setUser_id(note.getUser_id());
+            noteToUpdate.setAuthor(note.getAuthor());
+            noteToUpdate.setBody(note.getBody());
+            noteToUpdate.setDate_posted(note.getDate_posted());
+
+            System.out.println("Updating...");
+            return repository.save(noteToUpdate);
+
+        } else {
+            // We "force" to create a new note based on the note id and user id
+            // from the database
+            System.out.println("No resource found....creating now!");
+            Notes new_note = new Notes();
+            new_note.setNote_id(note_id);
+            new_note.setUser_id(user_id);
+            new_note.setAuthor(note.getAuthor());
+            new_note.setUser_id(note.getUser_id());
+            new_note.setBody(note.getBody());
+            new_note.setDate_posted(note.getDate_posted());
+
+            System.out.println("Updating...");
+            return repository.save(new_note);
         }
-        return repository.save(note);
     }
 
     @Override
